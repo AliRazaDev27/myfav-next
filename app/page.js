4b64244c2add1f8541144f1bb1b98d232ea675de
Ali Raza <alirazadev27@gmail.com>
2024-06-30 17:27:24 +0500

    BUG:broken due to switch from npm to pnpm

--- app/page.js
+++ app/page.js
@@ -1,40 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import Book from "@/lib/models/bookModel"
-import db, { clearDbCache } from "@/lib/db";
-import mongoose from "mongoose";
+import { Hero } from "@/components/hero";
 export default async function Home() {
-  const data = await getData();
-  async function getData() {
-    await db();
-    const books = await Book.find();
-    return books
-    clearDbCache();
-    return true
-  }
   return (
-    <div>HomePage
-
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Title</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={item._id}>
-                <td>{item.rank}</td>
-                <td>{item.title}</td>
-              </tr>
-            ))}
-
-          </tbody>
-        </table>
-
-      </div>
+    <div>
+      <Hero />
     </div >
   );
 }
