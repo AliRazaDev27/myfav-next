d33448b9fb036bcae52d51bd475325707535ac25
Ali Raza <alirazadev27@gmail.com>
2024-06-30 10:39:30 +0500

    fixed wrong db connection problem

--- app/page.js
+++ app/page.js
@@ -1,38 +1,41 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import db from "@/lib/db";
+import Book from "@/lib/models/bookModel"
+import db, { clearDbCache } from "@/lib/db";
 import mongoose from "mongoose";
-const dummySchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-}, { timestamps: true });
-const dummy = mongoose.models.dummy || mongoose.model("dummy", dummySchema);
 export default async function Home() {
   const data = await getData();
   async function getData() {
-    db();
-    const data = await dummy.find();
-    console.log(data);
-    return data
+    await db();
+    const books = await Book.find();
+    return books
+    clearDbCache();
+    return true
   }
   return (
     <div>HomePage
 
       <div>
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-          {data.map((item) => (
-            <tr key={item._id}>
-              <td>{item.name}</td>
-              <td>{item.age}</td>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Title</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item._id}>
+                <td>{item.rank}</td>
+                <td>{item.title}</td>
+              </tr>
+            ))}
+
+          </tbody>
         </table>
 
       </div>
-    </div>
+    </div >
   );
 }
+
