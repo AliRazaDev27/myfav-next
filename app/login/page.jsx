449df727e83007e76539fe40cb124c49a75bcacf
Ali Raza <alirazadev27@gmail.com>
2024-06-30 17:27:36 +0500

    BUG:broken due to switch from npm to pnpm

---
+++ app/login/page.jsx
@@ -0,0 +1,8 @@
+import { LoginForm } from '@/components/Login';
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center h-[90vh] bg-gray-700">
+      <LoginForm />
+    </main>
+  );
+}
